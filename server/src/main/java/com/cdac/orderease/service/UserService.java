ab7d9ffeed4db4e3a3de86c5989f36b1a6d8ba41
Saurabh Kumar <1355Saurabh@gmail.com>
2025-02-02 11:30:30 +0530

    User api's - getSingleUser - getAllUser - updateUser

--- server/src/main/java/com/cdac/orderease/service/UserService.java
+++ server/src/main/java/com/cdac/orderease/service/UserService.java
@@ -1,7 +1,10 @@
 package com.cdac.orderease.service;
 
+import java.util.List;
+
 import com.cdac.orderease.dto.LoginUserDTO;
 import com.cdac.orderease.dto.UserDTO;
+import com.cdac.orderease.exception.NoUsersFoundException;
 import com.cdac.orderease.exception.UserAlreadyPresentException;
 import com.cdac.orderease.exception.UserNotFoundException;
 
@@ -10,4 +13,10 @@ public interface UserService {
 	UserDTO registerUser(UserDTO userDto) throws UserAlreadyPresentException;
 
 	LoginUserDTO loginUser(LoginUserDTO loginUserDto) throws UserNotFoundException;
+
+	UserDTO getSingleUser(Long userId) throws UserNotFoundException;
+
+	List<UserDTO> getAllUsers() throws NoUsersFoundException;
+
+	UserDTO updateUser(Long userId, UserDTO userDto) throws UserNotFoundException;
 }
