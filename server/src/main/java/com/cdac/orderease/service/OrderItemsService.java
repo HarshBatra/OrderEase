d49f18ba8a30fac74ca76ed245ba68ad1289e250
Saurabh Kumar <1355Saurabh@gmail.com>
2025-02-04 16:32:28 +0530

    list of orderitems can be added.

--- server/src/main/java/com/cdac/orderease/service/OrderItemsService.java
+++ server/src/main/java/com/cdac/orderease/service/OrderItemsService.java
@@ -1,9 +1,13 @@
 package com.cdac.orderease.service;
 
+import java.util.List;
+
 import com.cdac.orderease.dto.OrderItemsDTO;
 
 public interface OrderItemsService {
 
 	OrderItemsDTO addOrderItems(OrderItemsDTO orderItemsDTO);
+
+	List<OrderItemsDTO> addListOfOrderItems(List<OrderItemsDTO> orderItemsDtoList);
 
 }
