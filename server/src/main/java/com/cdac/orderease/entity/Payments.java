2b675d89f90a3c01b5c1f7b1a533b43fd8b5974a
sudhansh <suudhanshjawale@gmail.com>
2025-02-05 11:51:43 +0530

    change amount to Double,make changes in repository,changes methods in payment mapper

--- server/src/main/java/com/cdac/orderease/entity/Payments.java
+++ server/src/main/java/com/cdac/orderease/entity/Payments.java
@@ -13,51 +13,81 @@ public class Payments {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long paymentId;
-	private Long orderid;
+	private Long orderId;
 	private String razorpayOrderId;
-	private Integer amount;
+	private Double amount;
 	private String paymentStatus;
+	
+	
 	public Long getPaymentId() {
 		return paymentId;
 	}
+
+
 	public void setPaymentId(Long paymentId) {
 		this.paymentId = paymentId;
 	}
-	public Long getOrderid() {
-		return orderid;
+
+
+	public Long getOrderId() {
+		return orderId;
 	}
-	public void setOrderid(Long orderid) {
-		this.orderid = orderid;
+
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
 	}
+
+
 	public String getRazorpayOrderId() {
 		return razorpayOrderId;
 	}
+
+
 	public void setRazorpayOrderId(String razorpayOrderId) {
 		this.razorpayOrderId = razorpayOrderId;
 	}
-	public Integer getAmount() {
+
+
+	public Double getAmount() {
 		return amount;
 	}
-	public void setAmount(Integer amount) {
+
+
+	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
+
+
 	public String getPaymentStatus() {
 		return paymentStatus;
 	}
+
+
 	public void setPaymentStatus(String paymentStatus) {
 		this.paymentStatus = paymentStatus;
 	}
+
 	
-	public Payments() {
-		super();
-	}
-	public Payments(Long paymentId, Long orderid, String razorpayOrderId, Integer amount, String paymentStatus) {
+	public Payments(Long paymentId, Long orderId, String razorpayOrderId, Double amount, String paymentStatus) {
 		super();
 		this.paymentId = paymentId;
-		this.orderid = orderid;
+		this.orderId = orderId;
 		this.razorpayOrderId = razorpayOrderId;
 		this.amount = amount;
 		this.paymentStatus = paymentStatus;
+	}
+	
+
+	public Payments() {
+		super();
+	}
+
+
+	@Override
+	public String toString() {
+		return "Payments [paymentId=" + paymentId + ", orderId=" + orderId + ", razorpayOrderId=" + razorpayOrderId
+				+ ", amount=" + amount + ", paymentStatus=" + paymentStatus + "]";
 	}
 	
 	
