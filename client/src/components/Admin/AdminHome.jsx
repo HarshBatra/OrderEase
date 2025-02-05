2a146885bb3e46a72bec3a8df670fc8dafdc156b
thearpitsarap <arpitsarap18@gmail.com>
2025-02-05 16:16:45 +0530

    added modal,routing to all orders

--- client/src/components/Admin/AdminHome.jsx
+++ client/src/components/Admin/AdminHome.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import MenuForm from './MenuForm'
 import MenuList from './MenuList'
+import { Link } from 'react-router'
 
 export const AdminHome = () => {
     return (
         <div>
-            <div className='flex justify-center mt-10 mb-10'>
-                <h1 className='font-bold text-4xl '>Menu Update</h1>
+            <div className='flex items-center justify-between mt-10 mb-10'>
+                <div className='flex-1 flex justify-center'>
+                    <h1 className='font-bold text-4xl'>Menu Update</h1>
+                </div>
+                <div className='flex items-center'>
+                    <Link to="/all-orders" className='bg-primary border rounded text-white p-1'>Show All Menu</Link>
+                </div>
             </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
                 <div className='p-5'>
                     <div className='flex md:m-5 justify-center '>
-                        <MenuForm/>
+                        <MenuForm />
                     </div>
                 </div>
                 <div className='p-3'>
                     <div className='flex justify-center'>
-                        <MenuList/>
+                        <MenuList />
                     </div>
                 </div>
             </div>
