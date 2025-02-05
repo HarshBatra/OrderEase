c715b0661a16596ea089f67dd233d1202d497a2c
sudhansh <suudhanshjawale@gmail.com>
2025-02-05 11:51:43 +0530

    change amount to Double,make changes in repository,changes methods in payment mapper

--- server/src/main/java/com/cdac/orderease/dto/PaymentsDTO.java
+++ server/src/main/java/com/cdac/orderease/dto/PaymentsDTO.java
@@ -1,60 +1,84 @@
 package com.cdac.orderease.dto;
 
 public class PaymentsDTO {
-    
-    private Long paymentId;
-    private Long orderId;
-    private String razorpayOrderId;
-    private Integer amount;
-    private String paymentStatus;
-    
-    public PaymentsDTO() {}
-    
-    public PaymentsDTO(Long paymentId, Long orderId, String razorpayOrderId, Integer amount, String paymentStatus) {
-        this.paymentId = paymentId;
-        this.orderId = orderId;
-        this.razorpayOrderId = razorpayOrderId;
-        this.amount = amount;
-        this.paymentStatus = paymentStatus;
-    }
+	
+	private Long paymentId;
+	private Long orderId;
+	private String razorpayOrderId;
+	private Double amount;
+	private String paymentStatus;
+	
+	
+	public Long getPaymentId() {
+		return paymentId;
+	}
 
-    public Long getPaymentId() {
-        return paymentId;
-    }
 
-    public void setPaymentId(Long paymentId) {
-        this.paymentId = paymentId;
-    }
+	public void setPaymentId(Long paymentId) {
+		this.paymentId = paymentId;
+	}
 
-    public Long getOrderId() {
-        return orderId;
-    }
 
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
+	public Long getOrderId() {
+		return orderId;
+	}
 
-    public String getRazorpayOrderId() {
-        return razorpayOrderId;
-    }
 
-    public void setRazorpayOrderId(String razorpayOrderId) {
-        this.razorpayOrderId = razorpayOrderId;
-    }
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
 
-    public Integer getAmount() {
-        return amount;
-    }
 
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
+	public String getRazorpayOrderId() {
+		return razorpayOrderId;
+	}
 
-    public String getPaymentStatus() {
-        return paymentStatus;
-    }
 
-    public void setPaymentStatus(String paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
+	public void setRazorpayOrderId(String razorpayOrderId) {
+		this.razorpayOrderId = razorpayOrderId;
+	}
+
+
+	public Double getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	
+	public PaymentsDTO(Long paymentId, Long orderId, String razorpayOrderId, Double amount, String paymentStatus) {
+		super();
+		this.paymentId = paymentId;
+		this.orderId = orderId;
+		this.razorpayOrderId = razorpayOrderId;
+		this.amount = amount;
+		this.paymentStatus = paymentStatus;
+	}
+	
+
+	public PaymentsDTO() {
+		super();
+	}
+
+
+	@Override
+	public String toString() {
+		return "Payments [paymentId=" + paymentId + ", orderId=" + orderId + ", razorpayOrderId=" + razorpayOrderId
+				+ ", amount=" + amount + ", paymentStatus=" + paymentStatus + "]";
+	}
+	
+	
 }
--- server/src/main/java/com/cdac/orderease/mapper/PaymentsMapper.java
+++ server/src/main/java/com/cdac/orderease/mapper/PaymentsMapper.java
@@ -4,27 +4,28 @@ import com.cdac.orderease.dto.PaymentsDTO;
 import com.cdac.orderease.entity.Payments;
 
 public class PaymentsMapper {
-    
-    public static PaymentsDTO toDTO(Payments payments) {
+    //mapOrderDtoToOrder
+//	mapPaymentsDTOtoPayments
+    public static PaymentsDTO mapPaymentstoPaymentsDTO(Payments payments) {
         if (payments == null) {
             return null;
         }
         return new PaymentsDTO(
             payments.getPaymentId(),
-            payments.getOrderid(),
+            payments.getOrderId(),
             payments.getRazorpayOrderId(),
             payments.getAmount(),
             payments.getPaymentStatus()
         );
     }
     
-    public static Payments toEntity(PaymentsDTO paymentsDTO) {
+    public static Payments mapPaymentsDTOtoPayments(PaymentsDTO paymentsDTO) {
         if (paymentsDTO == null) {
             return null;
         }
         Payments payments = new Payments();
         payments.setPaymentId(paymentsDTO.getPaymentId());
-        payments.setOrderid(paymentsDTO.getOrderId());
+        payments.setOrderId(paymentsDTO.getOrderId());
         payments.setRazorpayOrderId(paymentsDTO.getRazorpayOrderId());
         payments.setAmount(paymentsDTO.getAmount());
         payments.setPaymentStatus(paymentsDTO.getPaymentStatus());
