ab30440f928eeea8baa4f02015e0f50028190a29
thearpitsarap <arpitsarap18@gmail.com>
2025-02-08 17:01:28 +0530

    change

--- server/src/main/java/com/cdac/orderease/controller/UsersController.java
+++ server/src/main/java/com/cdac/orderease/controller/UsersController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import com.cdac.orderease.repository.UserRepository;
 import com.cdac.orderease.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")  
 public class UsersController {
 
 	@Autowired
